8ee21916efb1c85e70115255520b004b2941e7f9
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-10-22 10:19:42 +0530

    App

--- covid_19/src/App.js
+++ covid_19/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+
+//  import Cards from './components/Cards/Cards';
+//  import Charts from './components/Charts/Charts';
+//  import CounterPicker from './components/CounterPicker/CounterPicker';
+
+ import {Cards,Charts,CounterPicker} from './components';
+
 class App extends React.Component{
     render(){
         return(
