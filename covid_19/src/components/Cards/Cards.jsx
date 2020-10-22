ae90f7ed088c54ce80bbd584ffe01c959614b32e
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-10-22 16:59:08 +0530

    card

--- covid_19/src/components/Cards/Cards.jsx
+++ covid_19/src/components/Cards/Cards.jsx
@@ -3,15 +3,19 @@ import {Card,CardContent, Typography,Grid} from '@material-ui/core';
 
 import styles from './Cards.module.css';
 
-const Cards=(props)=>{
-    console.log(props);
-    return(
+const Cards=({confirmed,recovered,death,lastUpdate})=>{
+    console.log(confirmed);
+   if(!confirmed){
+       return 'Loading...';
+       
+   }
+    return( 
        <div className={styles.container}>
            <Grid container spacing={3} justify="center">
              <Grid item component={Card}>
                 <CardContent>
                     <Typography color="textSecondary" gutterbotto>Infected</Typography>
-                    <Typography variant="h5"> REAL DATA</Typography>
+                    <Typography variant="h5"> {confirmed.value}</Typography>
                     <Typography color="textSecondary">REAL DATE</Typography>
                     <Typography variant="body2">Number of active cases of COVI-19</Typography>
                 </CardContent>
