5b2f2a14a83caddf507d92a778686ac3e3e30276
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-10-22 12:10:27 +0530

    App

--- covid_19/src/App.js
+++ covid_19/src/App.js
@@ -6,17 +6,31 @@ import React from 'react';
 
  import {Cards,Charts,CounterPicker} from './components';
 import styles from './App.module.css';
-
+import {fetchData} from './api';
+//import fetchData from './api/index';
 
 class App extends React.Component{
+
+    state={
+      data:{},
+    }
+
+
+    async componentDidMount(){
+        const fetchedData=await fetchData();
+       this.setState({data:fetchedData});
+    }
+
     render(){
+
+        const {data}=this.state;
         return(
             <div className={styles.container}>
-                <Cards/>
+                <Cards data={data} />
                 <Charts/>
                 <CounterPicker/>
             </div>
-        )
+        );
     }
 }
 
--- covid_19/src/components/index.js
+++ covid_19/src/components/index.js
@@ -2,4 +2,4 @@ export {default as Cards} from './Cards/Cards';
 export {default as Charts} from './Charts/Charts';
 export {default as CounterPicker} from './CounterPicker/CounterPicker';
 
-
+//export {default as fetchData } from '../api/index'
